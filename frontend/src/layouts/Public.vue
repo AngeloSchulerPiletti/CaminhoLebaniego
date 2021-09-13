d1ed5a957911c58e9cda1b5079c6bad610df0c65
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-13 14:25:55 -0300

    backgrounds added

--- frontend/src/layouts/Public.vue
+++ frontend/src/layouts/Public.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="website_container">
     <div id="pic_background" :class="'background back-' + backID">
-      <header-component/>
+      <header-component />
       <section>
         <slot name="header_sec"></slot>
       </section>
@@ -23,13 +23,13 @@ export default {
       //
     };
   },
-  props:{
-      backID: String,
+  props: {
+    backID: String,
   },
-  components:{
-    'header-component': Header,
+  components: {
+    "header-component": Header,
   },
-}
+};
 </script>
 
 <style lang="scss">
@@ -38,11 +38,9 @@ export default {
     src: url('../assets/fonts/MonAmour.ttf');
 }
 
-// #website_container{
 
-// }
 #pic_background{
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   max-height: 80vw;
 }
@@ -54,5 +52,27 @@ export default {
 }
 .back-0{
   background-image: url('../assets/images/template/walkerman.jpg');
+}
+.back-1{
+  background: linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url("../assets/images/template/santiago.jpg");
+  background-size: cover;
+  background-position-y: bottom;
+  background-position-x: center;
+  background-repeat: no-repeat;
+}
+.back-2{
+  background-image: url("../assets/images/template/equipment.jpg");
+}
+.back-3{
+background-image: url("../assets/images/template/writter.jpg");
+  background-position-y: 80%;
+}
+.back-4{
+background-image: url("../assets/images/template/trekkingman.jpg");
+  background-position-y: top;
+}
+.back-5{
+background-image: url("../assets/images/template/mail.jpg");
+  background-position-y: top;
 }
 </style>
