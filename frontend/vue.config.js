2666f7435a327d8c7de0308b1ec94013e2dce9ca
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-15 13:47:47 -0300

    sass updated succesfully to be according new rules

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports={
     css: {
         loaderOptions:{
             sass:{
-                prependData: `@import "@/sass/_geralImports.scss";`,
+                prependData: `@use "@/sass" as *;`,
             }
         }
     }
