91942cd99f9d3eddc675b39d4f4c1111467460d5
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-14 23:16:04 -0300

    now, the imageSlider1 has animations

--- frontend/src/views/Caminho.vue
+++ frontend/src/views/Caminho.vue
@@ -13,28 +13,31 @@
         </div>
         <div class="paragraphs_slider">
           <p :class="pClass">
-            {{sliderParagraphs.content}}
+            {{ sliderParagraphs.content }}
           </p>
           <div class="controllers">
-            <arrow class="back arrow" @click="changeParagraph(-1)"/>
-            <arrow class="next arrow" @click="changeParagraph(1)"/>
+            <arrow :class="'back arrow '+pClass" @click="changeParagraph(-1)" />
+            <arrow :class="'next arrow '+pClass" @click="changeParagraph(1)" />
           </div>
         </div>
       </section>
       <section id="sec2">
-        <div class="img_container">
-          <img :src="'./images/pages/caminho/img'+sliderImages1.index+'.jpg'" alt="">
+        <div :class="'img_container ' + img1Class">
+          <img
+            :src="'./images/pages/caminho/img' + sliderImages1.index + '.jpg'"
+            alt=""
+          />
         </div>
         <div class="asider">
           <div class="top">
-            <h2>{{sliderImages1.title}}</h2>
-            <p>{{sliderImages1.paragraph}}</p>
+            <h2>{{ sliderImages1.title }}</h2>
+            <p>{{ sliderImages1.paragraph }}</p>
           </div>
           <div class="bottom">
-            <div class="btn_3" @click="changeImage(-1)">
+            <div :class="'btn_3 '+img1Class" @click="changeImage(-1)">
               Anterior
             </div>
-            <div class="btn_3" @click="changeImage(1)">
+            <div :class="'btn_3 '+img1Class" @click="changeImage(1)">
               Próxima
             </div>
           </div>
@@ -52,59 +55,80 @@ import Arrow from "@/components/SVGs/Arrow";
 export default {
   name: "caminho",
   inject: ["URL_API"],
-  data(){
-    return{
-      pClass:"",
+  data() {
+    return {
+      pClass: "",
+      img1Class: "",
       sliderParagraphs: {
         content: "",
         index: 0,
         tam: 0,
       },
-      sliderImages1:{
+      sliderImages1: {
         title: "",
         paragraph: "",
         index: 0,
         tam: 0,
       },
-    }
+    };
   },
-  mounted(){
+  mounted() {
     // console.log(this.pageData["sliderImages1"]);
   },
-  computed:{
-    availableParagraphs(){
-      this.sliderParagraphs.tam = this.pageData['sliderParagraphs'][1];
+  computed: {
+    availableParagraphs() {
+      this.sliderParagraphs.tam = this.pageData["sliderParagraphs"][1];
       if (this.sliderParagraphs.tam > 0) {
-        this.sliderParagraphs.content = Object.values(this.pageData['sliderParagraphs'][0])[this.sliderParagraphs.index];
+        this.sliderParagraphs.content = Object.values(
+          this.pageData["sliderParagraphs"][0]
+        )[this.sliderParagraphs.index];
       }
     },
-    availableImages1(){
+    availableImages1() {
       this.sliderImages1.tam = this.pageData["sliderImages1"][1];
       if (this.sliderImages1.tam > 0) {
-        this.sliderImages1.title = this.pageData["sliderImages1"][0][this.sliderImages1.index].title;
-        this.sliderImages1.paragraph = this.pageData["sliderImages1"][0][this.sliderImages1.index].paragraph;
+        this.sliderImages1.title = this.pageData["sliderImages1"][0][
+          this.sliderImages1.index
+        ].title;
+        this.sliderImages1.paragraph = this.pageData["sliderImages1"][0][
+          this.sliderImages1.index
+        ].paragraph;
       }
     },
   },
   watch: {
-    availableParagraphs(){},
-    availableImages1(){},
+    availableParagraphs() {},
+    availableImages1() {},
   },
   methods: {
-    changeParagraph(action){
+    changeParagraph(action) {
       this.pClass = "active";
       setTimeout(() => {
         var result = this.sliderParagraphs.index;
         result += action;
-        this.sliderParagraphs.index += (result >= 0) ? (result < this.sliderParagraphs.tam ? action : 0) : 0;
+        this.sliderParagraphs.index +=
+          result >= 0 ? (result < this.sliderParagraphs.tam ? action : 0) : 0;
         this.pClass = "";
-      }, 850);  
+      }, 850);
     },
-    changeImage(action){
+    changeImage(action) {
+      this.img1Class = "active";
+
+      setTimeout(() => {
         var result = this.sliderImages1.index;
         result += action;
-        this.sliderImages1.index += (result >= 0) ? (result < this.sliderImages1.tam ? action : -(this.sliderImages1.tam - 1)) : (this.sliderImages1.tam - 1);
-    }
+        this.sliderImages1.index +=
+          result >= 0
+            ? result < this.sliderImages1.tam
+              ? action
+              : -(this.sliderImages1.tam - 1)
+            : this.sliderImages1.tam - 1;
+       
+      }, 300); 
+      setTimeout(() => {
+          this.img1Class = "";
+      }, 600);
+    },
   },
   props: {
     pageData: Object,
@@ -137,19 +161,19 @@ export default {
       color: $white;
     }
   }
-  .paragraphs_slider{
+  .paragraphs_slider {
     padding: 0 10vw;
     margin: auto;
 
-    p{
-    margin-top: 2vw;
+    p {
+      margin-top: 2vw;
       position: relative;
       color: $white;
       padding: 10px 20px;
       display: flex;
       align-items: center;
 
-      &::before{
+      &::before {
         content: "";
         position: absolute;
         height: 100%;
@@ -160,84 +184,113 @@ export default {
 
         transition: right 500ms ease;
       }
-      &.active::before{
+      &.active::before {
         left: 0;
         right: 0;
       }
     }
-    .controllers{
+    .controllers {
       margin-top: 1vw;
       display: flex;
       gap: 10px;
       justify-content: flex-end;
 
-      &:deep(.arrow){
+      &:deep(.arrow) {
         cursor: pointer;
         width: 28px;
         height: 28px;
-        path{
+
+        &.active{
+          pointer-events: none;
+        }
+
+        path {
           fill: $white;
         }
 
         transition: transform 300ms;
       }
-      &:deep(.back):hover{
+      &:deep(.back):hover {
         transform: scale(1.1);
       }
-      &:deep(.next):hover{
+      &:deep(.next):hover {
         transform: scale(1.1) scaleX(-1);
       }
       // .back{
 
       // }
-      .next{
+      .next {
         transform: scale(1) scaleX(-1);
       }
     }
   }
 }
-#sec2{
+#sec2 {
   display: flex;
   gap: 5vw;
   margin-top: 8vw;
-  
-  .img_container{
+
+  .img_container {
     box-shadow: 0 0 10px 2px #000;
-    img{
-    width: 40vw;
-    height: 30vw;
+    position: relative;
+
+    &::before {
+      content: "";
+      position: absolute;
+      z-index: 100;
+      top: 0;
+      right: 0;
+      bottom: 100%;
+      left: 100%;
+
+      background-color: $red;
+      transition: bottom 300ms, left 300ms, top 300ms 300ms, right 300ms 300ms;
+    }
+    &.active::before {
+      bottom: 0;
+      left: 0;
+      top: 100%;
+      right: 100%;
+    }
+
+    img {
+      width: 40vw;
+      height: 30vw;
       object-fit: cover;
     }
-    
   }
-  .asider{
+  .asider {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     margin-right: 10vw;
     padding: 20px 0;
 
-    .top{
+    .top {
       color: $white;
       display: flex;
       flex-direction: column;
       gap: 20px;
 
-      h2{
+      h2 {
         @include Drawed1;
         font-size: 35px;
 
-        &::first-letter{
+        &::first-letter {
           color: $red;
         }
       }
-      p{
+      p {
         text-indent: 1em;
       }
     }
-    .bottom{
+    .bottom {
       display: flex;
       gap: 20px;
+
+      .btn_3.active{
+        pointer-events: none;
+      }
     }
   }
 }
