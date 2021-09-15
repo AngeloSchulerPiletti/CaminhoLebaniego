77c793c44336911d75a7c258f4519fac70417eb7
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-15 10:31:45 -0300

    vue-meta installed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,15 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueMeta from 'vue-meta'
 
-createApp(App).provide('URL_API', "http://127.0.0.1:8000/api/").use(store).use(router).use(VueAxios, axios).mount('#app')
+createApp(App)
+.provide('URL_API', "http://127.0.0.1:8000/api/")
+.use(store)
+.use(VueMeta, {
+    keyName: 'htmlMetaInfo',
+})
+.use(router)
+.use(VueAxios, axios)
+.mount('#app')
 
