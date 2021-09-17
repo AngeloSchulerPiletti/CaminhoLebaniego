f9c601d76dbea5131e172b3fbd4e405188b03be6
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-17 16:51:20 -0300

    Experiencia.vue created

--- frontend/src/views/Experiencia.vue
+++ frontend/src/views/Experiencia.vue
@@ -1,9 +1,19 @@
 <template>
   <app-layout backID="3">
     <template v-slot:header_sec>
-      <banner-content :pageData="pageData"/>
+      <banner-content :pageData="pageData" />
     </template>
-    <template v-slot:main><h1>ISSO É A main</h1></template>
+    <template v-slot:main>
+      <div class="top">
+        <h3 class="title4">1008,9Km em 32 dias</h3>
+        <p>
+          Entre 27 de maio e 01 de junho de 2018 fiz minha peregrinação à Santo
+          Toríbio, durante a Peregrinação a Santiago de Compostela pelo Caminho
+          do Norte.
+        </p>
+      </div>
+      <article-component :article="pageData.article"></article-component>
+    </template>
   </app-layout>
 </template>
 
@@ -11,13 +21,14 @@
 // @ is an alias to /src
 import AppLayout from "@/layouts/Public.vue";
 import BannerContent from "@/components/header/BannerContent.vue";
+import Article from "@/components/templates/Article.vue";
 
 export default {
   name: "experiencia",
   inject: ["URL_API"],
   methods: {},
-  created(){
-    this.$store.commit('setTitle', "Experiência");
+  created() {
+    this.$store.commit("setTitle", "Experiência");
   },
   props: {
     pageData: Object,
@@ -25,9 +36,25 @@ export default {
   components: {
     AppLayout,
     BannerContent,
+    "article-component": Article,
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.top {
+  h3 {
+    margin: 5vw 10vw 1.5vw 10vw;
+  }
+  p {
+    color: $white;
+    font-size: 17px;
+    margin: auto;
+    width: 80vw;
+    text-align: justify;
+    padding-bottom: 1.4vw;
+    margin-bottom: 2vw;
+    border-bottom: 2px solid $red;
+  }
+}
 </style>
