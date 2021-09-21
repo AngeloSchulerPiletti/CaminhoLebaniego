9523b3927512c9b3743b108295c71d95f7e155aa
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-21 16:52:50 -0300

    dashboard created

---
+++ frontend/src/views/admin/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <admin> DASHBOARD </admin>
+</template>
+
+<script>
+import Admin from "@/layouts/Admin";
+export default {
+  name: "dashboard",
+  components: {
+    Admin,
+  },
+};
+</script>
+
+<style></style>
