a957b9aa1c921b6abd109a25a8340cb2e1443653
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-22 11:05:46 -0300

    Login adapted to use new api service

--- frontend/src/views/admin/Login.vue
+++ frontend/src/views/admin/Login.vue
@@ -1,27 +1,31 @@
 <template>
   <admin>
-    <div>
+    <div id="login_container">
       <form @submit.prevent="submit(this)">
-        <div class="input_container">
-          <label for="email">Email</label>
-          <input
-            v-model="formUser.email"
-            type="email"
-            name="email"
-            id="email"
-          />
-        </div>
-        <div class="input_container">
-          <label for="password">Senha</label>
-          <input
-            v-model="formUser.password"
-            type="text"
-            name="password"
-            id="password"
-          />
+        <div class="inputs">
+          <div class="input_container">
+            <label for="email">Email</label>
+            <input
+              v-model="formUser.email"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="exemplo@gmail.com"
+            />
+          </div>
+          <div class="input_container">
+            <label for="password">Senha</label>
+            <input
+              v-model="formUser.password"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="SquarqsmasnaoSquins"
+            />
+          </div>
         </div>
         <div class="actions">
-          <button type="submit">Acessar</button>
+          <button :class="'btn_3 ' + disabled" type="submit">Acessar</button>
         </div>
       </form>
     </div>
@@ -30,7 +34,7 @@
 
 <script>
 import Admin from "@/layouts/Admin";
-import api from "@/service/api.js";
+import { api } from "@/service/api.js";
 
 export default {
   name: "login",
@@ -40,33 +44,25 @@ export default {
         email: "",
         password: "",
       },
+      disabled: "",
     };
   },
   methods: {
     submit: function() {
       console.log("submitado");
+      this.disabled = "disabled";
 
       api.post("login", this.formUser).then((response) => {
+        this.disabled = "";
+
         if (response.status >= 200 && response.status < 300) {
           this.$store.commit("setSessionData", {
             user: response.data.user,
             token: response.data.token,
           });
-          // this.$store.state.sessionData.user = response.data.user;
-          // this.$store.state.sessionData.token = response.data.token;
+
+          this.$router.push("/area-do-administrador");
         }
-
-        api.interceptors.request.use(
-          (config) => {
-            config.headers.Authorization = `Bearer ${this.$store.state.sessionData.token}`;
-            return config;
-          },
-          (error) => {
-            return Promise.reject(error);
-          }
-        );
-
-        api.get("teste").then(console.log);
       });
     },
   },
@@ -76,4 +72,55 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+#login_container {
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: fit-content;
+    margin: auto;
+    width: 50vw;
+    max-width: 400px;
+
+    .inputs {
+      display: flex;
+      flex-direction: column;
+      gap: 30px;
+      padding: 2vw;
+      background-color: $white;
+      border-radius: 10px;
+      box-shadow: 0px 0px 3vw 1.5vw $blackest;
+
+      .input_container {
+        @include Font0;
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+
+        label {
+          font-size: 14px;
+        }
+        input {
+          font-size: 16px;
+          padding: 4px;
+          border: none;
+          border-bottom: 1px solid $black;
+
+          &:target,
+          &:focus {
+            border-bottom: 1px solid $red;
+          }
+        }
+      }
+    }
+    .actions {
+      display: flex;
+      justify-content: space-between;
+
+      // button {
+      // }
+    }
+  }
+}
+</style>
