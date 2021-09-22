46fba69d6923f9f7896d3c1918d25b3ec9d6bcbb
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-22 11:04:25 -0300

    axios to no-auth request and auth request

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 
-const api = axios.create({
+export const api = axios.create({
     baseURL: "http://127.0.0.1:8000/api/",
 });
 
-export default api;
+export function deleteAuthorizationTokens(){
+    delete axios.defaults.headers.common["Authorization"];
+}
 
+export function setAuthorizationToken(token, axios){
+    axios.interceptors.request.use(
+        (config) => {
+          config.headers.Authorization = `Bearer ${token}`;
+          return config;
+        },
+        (error) => {
+          return Promise.reject(error);
+        }
+    );
+
+    return axios;
+}
