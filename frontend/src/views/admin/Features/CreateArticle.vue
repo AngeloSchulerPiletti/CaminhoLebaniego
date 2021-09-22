e0123707a181efb32035269f4b3e2ec54acd6f33
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-22 17:50:15 -0300

    form fields updated

--- frontend/src/views/admin/Features/CreateArticle.vue
+++ frontend/src/views/admin/Features/CreateArticle.vue
@@ -3,8 +3,31 @@
     <form @submit.prevent>
       <div class="inputs">
         <div class="input_container">
-          <label for=""></label>
-          <input type="text" />
+          <label for="title">Título do Artigo</label>
+          <input type="text" id="title" name="title" placeholder="Exemplo de Título"/>
+        </div>
+        <div class="input_container">
+          <label for="description">Descrição</label>
+          <textarea name="description" id="description" placeholder="Adicione aqui uma descrição para o seu artigo!"></textarea>
+        </div>
+        <div class="input_container">
+          <label for="text">Artigo</label>
+          <textarea name="text" id="text" placeholder="Escreva seu artigo conforme as regras de formatação pré-estabelecidas"></textarea>
+        </div>
+        <div class="input_container">
+          <label for="tags">TAGS do Artigo</label>
+          <input type="text" id="tags" name="tags" placeholder="Insira tags, separando-as por vírgula"/>
+        </div>
+        <div class="input_container">
+          <label for="draft">Salvar como Rascunho?</label>
+          <select name="draft" id="draft">
+            <option value="true">Sim</option>
+            <option value="false" selected>Não</option>
+          </select>
+        </div>
+        <div class="input_container">
+          <label for="images">Adicione as Imagens Zipadas ao Artigo</label>
+          <input type="file" name="images" id="images">
         </div>
       </div>
       <div class="actions">
