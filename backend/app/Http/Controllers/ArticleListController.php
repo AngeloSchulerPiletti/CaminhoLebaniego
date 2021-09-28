b71db6e1de22d5b7767aab238a77e31ce47b8512
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-28 17:40:55 -0300

    protecting deleted and drafted articles to be listed

--- backend/app/Http/Controllers/ArticleListController.php
+++ backend/app/Http/Controllers/ArticleListController.php
@@ -7,9 +7,14 @@ use Illuminate\Support\Facades\DB;
 
 class ArticleListController extends Controller
 {
-    static function getArticles($page = 1, $perpage = 10){
-        $articles_selection = DB::table('articles')->orderByDesc('id')->skip($perpage*($page - 1))->take($perpage)->get();
-
+    static function getArticles($page = 1, $perpage = 10)
+    {
+        if (!auth()->user()) {
+            $articles_selection = DB::table('articles')->where('status', 1)->orderByDesc('updated_at')->skip($perpage * ($page - 1))->take($perpage)->get();
+        } else {
+            $articles_selection = DB::table('articles')->orderByDesc('updated_at')->skip($perpage * ($page - 1))->take($perpage)->get();
+        }
+        
         return $articles_selection;
     }
 }
