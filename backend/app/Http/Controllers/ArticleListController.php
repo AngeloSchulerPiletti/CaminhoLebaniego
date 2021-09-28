01de4d919b1279df35e6e04f920be2063aba8fcb
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-28 17:58:38 -0300

    changing to getArticles return only articles with status 1

--- backend/app/Http/Controllers/ArticleListController.php
+++ backend/app/Http/Controllers/ArticleListController.php
@@ -9,11 +9,7 @@ class ArticleListController extends Controller
 {
     static function getArticles($page = 1, $perpage = 10)
     {
-        if (!auth()->user()) {
-            $articles_selection = DB::table('articles')->where('status', 1)->orderByDesc('updated_at')->skip($perpage * ($page - 1))->take($perpage)->get();
-        } else {
-            $articles_selection = DB::table('articles')->orderByDesc('updated_at')->skip($perpage * ($page - 1))->take($perpage)->get();
-        }
+        $articles_selection = DB::table('articles')->where('status', 1)->orderByDesc('updated_at')->skip($perpage * ($page - 1))->take($perpage)->get();
         
         return $articles_selection;
     }
