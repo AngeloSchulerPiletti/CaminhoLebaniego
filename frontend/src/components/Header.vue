22f5e3da42025589841999c2eb7d33f55b4aa7cc
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-29 11:41:07 -0300

    modal search created

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,51 +1,67 @@
 <template>
-<div>
+  <div>
     <div v-if="$store.state.logged" id="admin_header">
       <p>
-      Bem vindo, {{$store.state.sessionData.user.name}}. Você é admin e está logado!
+        Bem vindo, {{ $store.state.sessionData.user.name }}. Você é admin e está
+        logado!
       </p>
-      <p @click="$router.push({name: 'admin'})">
+      <p @click="$router.push({ name: 'admin' })">
         Ir para a área do admin
       </p>
     </div>
-  <header>
-    <div id="logo_container">
-      <router-link to="/">
-        <header-logo />
-      </router-link>
-    </div>
-    <nav id="header_nav">
-      <ul>
-        <router-link to="/caminho"><li class="hover1">O Caminho</li></router-link>
-        <router-link to="/preparacao"><li class="hover1">Preparação</li></router-link>
-        <router-link to="/experiencia"><li class="hover1">Experiência</li></router-link>
-        <router-link to="/artigos"><li class="hover1">Artigos</li></router-link>
-        <router-link to="/contato"><li class="hover1">Contato</li></router-link>
-      </ul>
-    </nav>
-  </header>
-
-</div>
+    <header>
+      <div id="logo_container">
+        <router-link to="/">
+          <header-logo />
+        </router-link>
+      </div>
+      <nav id="header_nav">
+        <ul>
+          <router-link to="/caminho"
+            ><li class="hover1">O Caminho</li></router-link
+          >
+          <router-link to="/preparacao"
+            ><li class="hover1">Preparação</li></router-link
+          >
+          <router-link to="/experiencia"
+            ><li class="hover1">Experiência</li></router-link
+          >
+          <router-link to="/artigos"
+            ><li class="hover1">Artigos</li></router-link
+          >
+          <router-link to="/contato"
+            ><li class="hover1">Contato</li></router-link
+          >
+        </ul>
+        <div class="search_btn">
+          <search-icon />
+        </div>
+      </nav>
+    </header>
+  </div>
 </template>
 
 <script>
 import HeaderLogo from "@/components/SVGs/HeaderLogo.vue";
+import SearchIcon from "@/components/SVGs/SearchIcon";
+
 export default {
   components: {
     HeaderLogo,
+    SearchIcon,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-#admin_header{
+#admin_header {
   background-color: $red;
   color: $white;
   display: flex;
   justify-content: space-between;
   padding: 3px 12px;
 
-  p:nth-child(2){
+  p:nth-child(2) {
     text-decoration: underline;
     cursor: pointer;
   }
@@ -54,10 +70,10 @@ export default {
 $pad_top: 1vw;
 header {
   display: grid;
-  grid-template-columns: 4fr 9fr;
+  grid-template-columns: 5fr 14fr;
   gap: 5vw;
 
-  padding: 0 10vw 5vw 10vw;
+  padding: 0 6vw 5vw 10vw;
 
   background: linear-gradient(180deg, #000000e0 40%, #00000000);
 
@@ -65,7 +81,12 @@ header {
     @include headerLogo($pad_top);
   }
   nav {
+    display: flex;
+    align-items: center;
+    gap: 6vw;
+
     ul {
+      flex-grow: 1;
       display: flex;
       align-items: center;
       justify-content: space-around;
@@ -91,6 +112,35 @@ header {
         @include Font1;
 
         position: relative;
+      }
+    }
+    .search_btn {
+      height: 30px;
+      width: 30px;
+      cursor: pointer;
+
+      svg {
+        height: 100%;
+        border-bottom: 1px solid transparent;
+        padding: 4px;
+        transition: border-bottom 300ms;
+
+        &:deep() {
+          path {
+            fill: $white;
+            transition: fill 500ms;
+          }
+        }
+      }
+
+      &:hover {
+        svg {
+          border-bottom: 1px solid $red;
+
+          &:deep(path) {
+            fill: $red;
+          }
+        }
       }
     }
   }
