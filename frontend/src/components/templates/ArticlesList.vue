d6185fc59bb49203a03104d4660cadb940afc708
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-29 11:17:37 -0300

    ArticlesList.vue styled

--- frontend/src/components/templates/ArticlesList.vue
+++ frontend/src/components/templates/ArticlesList.vue
@@ -1,28 +1,46 @@
 <template>
   <section class="wrapper">
+    <div class="title" v-if="title">
+      <h1 class="title2">{{ title }}</h1>
+    </div>
     <div v-if="articlesList.length < 1">
-        Tivemos um problema ao carregar os artigos
+      Tivemos um problema ao carregar os artigos
     </div>
     <div v-else-if="Object.keys(articlesList) < 1">
-        Carregando...
+      Carregando...
     </div>
     <div v-else class="list_container">
-        <div class="card" v-for="(article, index) in articlesList" :key="index">
-          <div class="content_container" @click="goToArticle(article.url)">
-            <h2>{{article.title}}</h2>
-            <p>{{article.description}}</p>
-            <div class="tags container">
-              <span @click="searchFor(tag)" v-for="tag in article.tags" :key="tag">{{tag}}</span>
-            </div>
-          </div>
-          <div v-if="$store.state.logged && canControll" class="controll_container">
-            <ul>
-              <li @click="callControll('del')">Deletar</li>
-              <li @click="callControll('viz')">Vizualizar</li>
-              <li @click="callControll('edi')">Editar</li>
-            </ul>
+      <div
+        :class="'card ' + canControll"
+        v-for="(article, index) in articlesList"
+        :key="index"
+      >
+        <div class="content_container">
+          <h2 class="title3-2" @click="goToArticle(article.url)">
+            {{ article.title }}
+          </h2>
+          <p class="simple_p">{{ article.description }}</p>
+          <div class="tags_container">
+            <span
+              class="badge_1"
+              @click="searchFor(tag)"
+              v-for="tag in article.tags"
+              :key="tag"
+              >{{ tag }}</span
+            >
           </div>
         </div>
+        <div
+          v-if="$store.state.logged && canControll"
+          class="controll_container"
+        >
+          <ul>
+            <li @click="callControll('del')">Deletar</li>
+            <li @click="callControll('viz')">Vizualizar</li>
+            <li @click="callControll('edi')">Editar</li>
+          </ul>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -46,32 +64,119 @@ export default {
       apiRequestProtocol()
         .get(`lista-de-artigos/${this.page}/${this.perpage}`)
         .then((response) => {
-            this.articlesList = response.data;
-            console.log(response.data);
+          this.articlesList = response.data;
+          console.log(response.data);
         })
         .catch((error) => {
-            this.articlesList = false;
+          this.articlesList = false;
         });
     },
-    goToArticle(url){
-        this.$router.push({name: 'artigo-show', query: { titulo: url }});
+    goToArticle(url) {
+      this.$router.push({ name: "artigo-show", query: { titulo: url } });
     },
-    searchFor(tag){
-      console.log('asking to search for '+tag);
+    searchFor(tag) {
+      console.log("asking to search for " + tag);
     },
-    callControll(action){
-      console.log('asking for '+action+' article');
+    callControll(action) {
+      console.log("asking for " + action + " article");
     },
   },
   props: {
     canControll: Boolean,
+    title: String,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.card{
-  background-color: white;
-  margin: 5vw 0;
+.title {
+  margin: 0 5vw;
+}
+.list_container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3vw;
+  padding: 5vw 0;
+
+  .card {
+    box-shadow: 0 0 0.8vw 0.3vw #000;
+    width: 80%;
+    display: flex;
+    padding: 1vw;
+    background-color: darken($white, 5%);
+
+    .content_container {
+      display: flex;
+      flex-direction: column;
+      gap: 1vw;
+      h2 {
+        cursor: pointer;
+      }
+      p {
+        cursor: default;
+      }
+      .tags_container {
+        display: flex;
+        gap: 1vw;
+        span {
+          font-size: 12px;
+          background-color: $red;
+          color: $white;
+        }
+      }
+    }
+    .controll_container {
+      border-left: 2px solid $red;
+      padding-left: 1vw;
+      margin-left: 1vw;
+
+      ul {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 1vw;
+        height: 100%;
+
+        li {
+          cursor: pointer;
+          @include Font0;
+
+          &:hover {
+            color: $red;
+            text-decoration: underline;
+          }
+        }
+      }
+    }
+
+    &.false {
+      position: relative;
+      &::after,
+      &::before {
+        content: "";
+        position: absolute;
+        height: 80%;
+        top: 50%;
+        transform: translateY(-50%);
+        border: 1px solid $red;
+        z-index: -1;
+        width: 10vw;
+        transition: left 200ms ease-in, right 200ms ease-in;
+      }
+      &::before {
+        left: 0;
+      }
+      &::after {
+        right: 0;
+      }
+      &:hover::before {
+        left: -4vw;
+      }
+      &:hover::after {
+        right: -4vw;
+      }
+    }
+  }
 }
 </style>
