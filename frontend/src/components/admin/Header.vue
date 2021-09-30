e8fd5c9f141ef683f979a861465505f87fb900c2
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-09-30 17:58:17 -0300

    alterar páginas button disabled

--- frontend/src/components/admin/Header.vue
+++ frontend/src/components/admin/Header.vue
@@ -11,7 +11,7 @@
       </button>
       <button class="btn_4" @click="$router.push({name: 'criar_artigos'})" v-if="logged">criar artigo</button>
       <button class="btn_4" @click="$router.push({name: 'alterar_artigos'})" v-if="logged">alterar artigos</button>
-      <button class="btn_4" @click="$router.push({name: 'alterar_paginas'})" v-if="logged">alterar páginas</button>
+      <!-- <button class="btn_4" @click="$router.push({name: 'alterar_paginas'})" v-if="logged">alterar páginas</button> -->
       <button class="btn_4" @click="logout" v-if="logged">logout</button>
     </div>
   </header>
