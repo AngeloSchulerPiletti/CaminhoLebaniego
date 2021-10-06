d3d9e105f7980d464b2435249d8b7a61c33c8891
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-10-06 18:39:02 -0300

    error page styled

--- frontend/src/views/error/Error.vue
+++ frontend/src/views/error/Error.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-  <slot></slot>
+  <div class="error-view">
+    <div class="content-default">
+      <h1 class="title4 error-code">{{ error.code }}</h1>
+      <p class="error-message">{{ error.message }}</p>
     </div>
+    <div class="options">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    error: Object,
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.error-view {
+  position: absolute;
+  background: url("http://localhost:8000/images/errors/404/Camino.jpg");
+  background-size: cover;
+  background-position-x: 50%;
 
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .content-default {
+    padding: 5vw 4vw 7vw 4vw;
+    background: linear-gradient(180deg, #000000C9 20%, #00000090 65%,#00000000);
+
+    .error-code {
+      width: 100%;
+    }
+    .error-message {
+      color: $white;
+      margin-top: 1em;
+    }
+  }
+
+  .options{
+    padding: 50px 14vw 50px 4vw;
+    background: radial-gradient(
+      closest-side at 40%,
+      rgba(0, 0, 0, 0.6) 35%,
+      rgba(0, 0, 0, 0.4) 55%,
+      rgba(0, 0, 0, 0.15) 85%,
+      transparent
+    );
+    width: fit-content;
+  }
+}
 </style>
--- frontend/src/views/error/Error404.vue
+++ frontend/src/views/error/Error404.vue
@@ -1,25 +1,81 @@
 <template>
-    <error>
-        <div>
-            <h1>ERROR 404</h1>
-        </div>
-    </error>
+  <error :error="error">
+    <h6 class="title">O que você pode fazer:</h6>
+    <ul>
+      <li @click="$router.go(-2)">Voltar</li>
+      <li @click="$router.push('/')">Página inicial</li>
+    </ul>
+  </error>
 </template>
 
 <script>
 import Error from "@/views/error/Error";
 
 export default {
-    name: "error404",
-  created(){
-    this.$store.commit('setTitle', "Erro 404");
+  data() {
+    return {
+      error: {
+        code: "Erro 404",
+        message: "Perdão, mas a página solicitada não foi encontrada!",
+      },
+    };
   },
-components: {
+  created() {
+    this.$store.commit("setTitle", "Erro 404");
+  },
+  components: {
     Error,
-},
-}
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+h6{
+    margin-bottom: 30px;
+    width: fit-content;
+    @include Title2;
+    color: $white;    
+    font-size: 22px;
+    position: relative;
+    padding-bottom: 4px;
 
+    &::before{
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        border-bottom: 1px solid $red;
+        width: 20%;
+    }
+}
+ul {
+  @include Font1;
+  margin-left: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+
+  li {
+    cursor: pointer;
+    transition: color 300ms;
+    color: $white;
+    position: relative;
+    padding-left: 8px;
+
+    &::before {
+      content: "";
+      position: absolute;
+      border-left: 2px solid $white;
+      height: 100%;
+      left: 0;
+      top: 0;
+
+      transition: border-left 300ms;
+    }
+
+    &:hover::before {
+      border-left: 2px solid $red;
+    }
+  }
+}
 </style>
